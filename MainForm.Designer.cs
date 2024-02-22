dfbe7b74cfbc5a97ac852a4760e81bd49636a86e
Frank Gines <frnkgines@gmail.com>
2024-02-22 20:42:10 +0800

    Data Visualization Added

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -30,7 +30,8 @@
         {
             this.components = new System.ComponentModel.Container();
             this.panelSide = new System.Windows.Forms.Panel();
-            this.btn_print = new FontAwesome.Sharp.IconButton();
+            this.btn_prnt = new FontAwesome.Sharp.IconButton();
+            this.btn_dataststcs = new FontAwesome.Sharp.IconButton();
             this.btn_reg = new FontAwesome.Sharp.IconButton();
             this.btn_attendance = new FontAwesome.Sharp.IconButton();
             this.panel1 = new System.Windows.Forms.Panel();
@@ -45,7 +46,8 @@
             // panelSide
             // 
             this.panelSide.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(55)))), ((int)(((byte)(61)))));
-            this.panelSide.Controls.Add(this.btn_print);
+            this.panelSide.Controls.Add(this.btn_prnt);
+            this.panelSide.Controls.Add(this.btn_dataststcs);
             this.panelSide.Controls.Add(this.btn_reg);
             this.panelSide.Controls.Add(this.btn_attendance);
             this.panelSide.Controls.Add(this.panel1);
@@ -55,29 +57,53 @@
             this.panelSide.Size = new System.Drawing.Size(284, 653);
             this.panelSide.TabIndex = 0;
             // 
-            // btn_print
+            // btn_prnt
             // 
-            this.btn_print.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(55)))), ((int)(((byte)(61)))));
-            this.btn_print.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btn_print.FlatAppearance.BorderSize = 0;
-            this.btn_print.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_print.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_print.ForeColor = System.Drawing.Color.White;
-            this.btn_print.IconChar = FontAwesome.Sharp.IconChar.Print;
-            this.btn_print.IconColor = System.Drawing.Color.White;
-            this.btn_print.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.btn_print.IconSize = 35;
-            this.btn_print.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btn_print.Location = new System.Drawing.Point(0, 191);
-            this.btn_print.Margin = new System.Windows.Forms.Padding(0);
-            this.btn_print.Name = "btn_print";
-            this.btn_print.Padding = new System.Windows.Forms.Padding(40, 0, 0, 0);
-            this.btn_print.Size = new System.Drawing.Size(284, 60);
-            this.btn_print.TabIndex = 3;
-            this.btn_print.Text = "  Print";
-            this.btn_print.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.btn_print.UseVisualStyleBackColor = false;
-            this.btn_print.Click += new System.EventHandler(this.btn_print_Click);
+            this.btn_prnt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(55)))), ((int)(((byte)(61)))));
+            this.btn_prnt.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_prnt.FlatAppearance.BorderSize = 0;
+            this.btn_prnt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_prnt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_prnt.ForeColor = System.Drawing.Color.White;
+            this.btn_prnt.IconChar = FontAwesome.Sharp.IconChar.Print;
+            this.btn_prnt.IconColor = System.Drawing.Color.White;
+            this.btn_prnt.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btn_prnt.IconSize = 35;
+            this.btn_prnt.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_prnt.Location = new System.Drawing.Point(0, 251);
+            this.btn_prnt.Margin = new System.Windows.Forms.Padding(0);
+            this.btn_prnt.Name = "btn_prnt";
+            this.btn_prnt.Padding = new System.Windows.Forms.Padding(40, 0, 0, 0);
+            this.btn_prnt.Size = new System.Drawing.Size(284, 60);
+            this.btn_prnt.TabIndex = 4;
+            this.btn_prnt.Text = "Print";
+            this.btn_prnt.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_prnt.UseVisualStyleBackColor = false;
+            this.btn_prnt.Click += new System.EventHandler(this.iconButton1_Click);
+            // 
+            // btn_dataststcs
+            // 
+            this.btn_dataststcs.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(55)))), ((int)(((byte)(61)))));
+            this.btn_dataststcs.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_dataststcs.FlatAppearance.BorderSize = 0;
+            this.btn_dataststcs.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_dataststcs.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_dataststcs.ForeColor = System.Drawing.Color.White;
+            this.btn_dataststcs.IconChar = FontAwesome.Sharp.IconChar.Database;
+            this.btn_dataststcs.IconColor = System.Drawing.Color.White;
+            this.btn_dataststcs.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btn_dataststcs.IconSize = 35;
+            this.btn_dataststcs.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_dataststcs.Location = new System.Drawing.Point(0, 191);
+            this.btn_dataststcs.Margin = new System.Windows.Forms.Padding(0);
+            this.btn_dataststcs.Name = "btn_dataststcs";
+            this.btn_dataststcs.Padding = new System.Windows.Forms.Padding(40, 0, 0, 0);
+            this.btn_dataststcs.Size = new System.Drawing.Size(284, 60);
+            this.btn_dataststcs.TabIndex = 3;
+            this.btn_dataststcs.Text = "Data Statistics";
+            this.btn_dataststcs.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_dataststcs.UseVisualStyleBackColor = false;
+            this.btn_dataststcs.Click += new System.EventHandler(this.btn_print_Click);
             // 
             // btn_reg
             // 
@@ -85,7 +111,7 @@
             this.btn_reg.Dock = System.Windows.Forms.DockStyle.Top;
             this.btn_reg.FlatAppearance.BorderSize = 0;
             this.btn_reg.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_reg.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_reg.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_reg.ForeColor = System.Drawing.Color.White;
             this.btn_reg.IconChar = FontAwesome.Sharp.IconChar.UserGroup;
             this.btn_reg.IconColor = System.Drawing.Color.White;
@@ -109,7 +135,7 @@
             this.btn_attendance.Dock = System.Windows.Forms.DockStyle.Top;
             this.btn_attendance.FlatAppearance.BorderSize = 0;
             this.btn_attendance.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_attendance.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_attendance.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_attendance.ForeColor = System.Drawing.Color.White;
             this.btn_attendance.IconChar = FontAwesome.Sharp.IconChar.IdCard;
             this.btn_attendance.IconColor = System.Drawing.Color.White;
@@ -140,7 +166,7 @@
             // 
             this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(44)))), ((int)(((byte)(48)))));
             this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label1.Font = new System.Drawing.Font("Century Gothic", 10F);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
@@ -169,17 +195,18 @@
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1087, 653);
             this.Controls.Add(this.panelMain);
             this.Controls.Add(this.panelTop);
             this.Controls.Add(this.panelSide);
-            this.Font = new System.Drawing.Font("Century Gothic", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.Black;
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.panelSide.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
@@ -195,9 +222,10 @@
         private System.Windows.Forms.Panel panel1;
         private FontAwesome.Sharp.IconButton btn_attendance;
         private System.Windows.Forms.Label label1;
-        private FontAwesome.Sharp.IconButton btn_print;
+        private FontAwesome.Sharp.IconButton btn_dataststcs;
         private FontAwesome.Sharp.IconButton btn_reg;
         private System.Windows.Forms.Timer timer;
+        private FontAwesome.Sharp.IconButton btn_prnt;
     }
 }
 
